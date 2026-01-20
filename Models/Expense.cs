d42ae60a44f0e4ff3e28bccf58491dc0f9b55337
Thanos Papageorgiou <thanos.p012@gmail.com>
2026-01-21 00:47:44 +0200

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpTracker.Models
+{
+    public class Expense
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string Note { get; set; }
+    }
+}
