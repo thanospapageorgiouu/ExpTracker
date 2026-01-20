f76b43efe78a3e6d5cdf927573c1a1fea1c87e3b
Thanos Papageorgiou <thanos.p012@gmail.com>
2026-01-21 01:34:37 +0200

    Add JSON loading service for expenses

---
+++ Services/ExpenseStorageService.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using ExpTracker.Models;
+
+namespace ExpTracker.Services
+{
+    public class ExpenseStorageService
+    {
+        private readonly string _filePath = "expenses.json";
+
+        public List<Expense> LoadExpenses()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Expense>();
+
+            string json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Expense>>(json)
+                   ?? new List<Expense>();
+        }
+    }
+}
